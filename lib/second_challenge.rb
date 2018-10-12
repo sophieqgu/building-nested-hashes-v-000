940e7b9590c1d2d0c2b074d53bd467bf7b6c87f2
Sophie Gu <sophieqgu@gmail.com>
2018-10-12 14:21:42 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,7 +1,31 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      #your key/value pairs here!
+    :patriarch => {
+          :name => "Lord Montague",
+          :age => "53",
+        },
+        :matriarch => {
+          :name => "Lady Montague",
+          :age => "54",
+        },
+        :hero => {
+          :name => "Romeo",
+          :age => "15",
+          :status => "alive"
+        },
+        :hero_friends => [
+          {
+          :name => "Benvolio",
+          :age => "17",
+          :attitude => "worried"
+          }, 
+          {
+          :name => "Mercutio",
+          :age => "18",
+          :attitude => "hot-headed"
+          }
+        ]
       }, 
     :capulet => {
       #your key/value pairs here!
