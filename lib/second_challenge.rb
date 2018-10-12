f2a6c416b4d56accf1e324c13759d2d2dad6cf01
Sophie Gu <sophieqgu@gmail.com>
2018-10-12 14:26:29 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -16,8 +16,7 @@ def second_challenge
       }, 
     :capulet => {
         :patriarch => {
-          :name => "Lord Capulet",
-          :age => "50"
+         
         },
         :matriarch => {
           :name => "Lady Capulet", 
