0a71bc0a1db6f8718382d312c07de2d0870bed41
Sophie Gu <sophieqgu@gmail.com>
2018-10-12 14:26:01 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -11,16 +11,7 @@ def second_challenge
           
         },
         :hero_friends => [
-          {
-          :name => "Benvolio",
-          :age => "17",
-          :attitude => "worried"
-          }, 
-          {
-          :name => "Mercutio",
-          :age => "18",
-          :attitude => "hot-headed"
-          }
+          
         ]
       }, 
     :capulet => {
